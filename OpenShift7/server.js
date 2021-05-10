afdafdf51ae0abafb4d3b28456a4b27cd00e3ab7
victortacu5 <83407924+victortacu5@users.noreply.github.com>
2021-05-10 11:44:13 +0300

    Add files via upload

---
+++ OpenShift7/server.js
@@ -0,0 +1,25 @@
+var mysql = require('mysql');
+process.on('uncaughtException', (err) => {
+ console.log(`Caught exception: ${err}`);
+});
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (req, res) =>{
+var con = mysql.createConnection({
+ host: ,
+ user: ,
+ password: ,
+ database: 
+});
+con.connect(function(err) {
+ if (err) throw err;
+ console.log("Connected!");
+});
+con.query('SELECT * from test2', function (err, rows, fields) {
+ if (err) res.send(err)
+ console.log('The solution is: ', rows)
+ res.send(rows)
+})
+ })
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
