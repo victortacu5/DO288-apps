d52bce4c029d1ba8e3634c2532e2817baa352446
victortacu5 <83407924+victortacu5@users.noreply.github.com>
2021-05-11 10:04:09 +0300

    Update server.js

--- OpenShift7/server.js
+++ OpenShift7/server.js
@@ -7,7 +7,7 @@ const app = express()
 const port = 3000
 app.get('/', (req, res) =>{
 var con = mysql.createConnection({
- host: 172.30.169.26,
+ host: localhost,
  user: victor,
  password: victor,
  database: test
